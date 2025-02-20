180ccd5a063e51e79aa1f1036f4db361b28745c0
tiefseetauchner <lena@tauchner.me>
2025-02-20 04:24:44 +0100

    - rewrite keyboard control

--- lib/ui/screens/prompter_screen.dart
+++ lib/ui/screens/prompter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tiefprompt/core/theme.dart';
 import 'package:tiefprompt/providers/prompter_provider.dart';
 import 'package:tiefprompt/providers/settings_provider.dart';
@@ -58,52 +59,70 @@ class _PrompterScreenState extends ConsumerState<PrompterScreen> {
     return KeyboardListener(
       onKeyEvent: (keyEvent) {
         if (keyEvent is KeyDownEvent) {
-          switch (keyEvent.logicalKey) {
-            case LogicalKeyboardKey.space:
+          switch (keyEvent.physicalKey) {
+            case PhysicalKeyboardKey.space:
               ref.read(prompterProvider.notifier).togglePlayPause();
               break;
-            case LogicalKeyboardKey.arrowUp:
+            case PhysicalKeyboardKey.arrowUp:
               _scrollableTextController.scrollController.jumpTo(
                   _scrollableTextController.scrollController.offset - 75);
               break;
-            case LogicalKeyboardKey.arrowDown:
+            case PhysicalKeyboardKey.arrowDown:
               _scrollableTextController.scrollController.jumpTo(
                   _scrollableTextController.scrollController.offset + 75);
               break;
-            case LogicalKeyboardKey.pageUp:
+            case PhysicalKeyboardKey.pageUp:
               _scrollableTextController.scrollController.jumpTo(
                   _scrollableTextController.scrollController.offset -
                       MediaQuery.of(context).size.height);
               break;
-            case LogicalKeyboardKey.pageDown:
+            case PhysicalKeyboardKey.pageDown:
               _scrollableTextController.scrollController.jumpTo(
                   _scrollableTextController.scrollController.offset +
                       MediaQuery.of(context).size.height);
               break;
-            case LogicalKeyboardKey.home:
+            case PhysicalKeyboardKey.home:
               _scrollableTextController.scrollController.jumpTo(0);
               break;
-            case LogicalKeyboardKey.end:
+            case PhysicalKeyboardKey.end:
               _scrollableTextController.scrollController.jumpTo(
                   _scrollableTextController
                       .scrollController.position.maxScrollExtent);
               break;
-            case LogicalKeyboardKey.tab:
+            case PhysicalKeyboardKey.tab:
               ref.read(controlsVisibleProvider.notifier).state =
                   !controlsVisible;
               break;
-            case LogicalKeyboardKey.add:
-            case LogicalKeyboardKey.numpadAdd:
-              ref.read(prompterProvider.notifier).increaseSpeed(1);
+            case PhysicalKeyboardKey.equal:
+            case PhysicalKeyboardKey.numpadAdd:
+              if (HardwareKeyboard.instance.isControlPressed) {
+                ref.read(prompterProvider.notifier).increaseFontSize(10);
+              } else {
+                ref.read(prompterProvider.notifier).increaseSpeed(1);
+              }
               break;
-            case LogicalKeyboardKey.minus:
-            case LogicalKeyboardKey.numpadSubtract:
-              ref.read(prompterProvider.notifier).decreaseSpeed(1);
+            case PhysicalKeyboardKey.minus:
+            case PhysicalKeyboardKey.numpadSubtract:
+              if (HardwareKeyboard.instance.isControlPressed) {
+                ref.read(prompterProvider.notifier).decreaseFontSize(10);
+              } else {
+                ref.read(prompterProvider.notifier).decreaseSpeed(1);
+              }
               break;
-            case LogicalKeyboardKey.save:
-              ref
-                  .read(settingsProvider.notifier)
-                  .applySettingsFromPrompter(prompter);
+            case PhysicalKeyboardKey.period:
+              if (HardwareKeyboard.instance.isControlPressed) {
+                context.push('/settings');
+              }
+              break;
+          }
+
+          switch (keyEvent.character) {
+            case "s":
+              if (HardwareKeyboard.instance.isControlPressed) {
+                ref
+                    .read(settingsProvider.notifier)
+                    .applySettingsFromPrompter(prompter);
+              }
               break;
           }
         }
