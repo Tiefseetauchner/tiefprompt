87bf1fdbdd6f098dead14cd2ffd9a68b26444e23
tiefseetauchner <lena@tauchner.me>
2025-08-02 18:48:57 +0200

    - revert ventures to do the thing

--- linux/runner/main.cc
+++ linux/runner/main.cc
@@ -1,8 +1,6 @@
 #include "my_application.h"
 
 int main(int argc, char** argv) {
-  SetEnvironmentVariable(L"FLUTTER_TARGET", L"lib/linux_main.dart");
-
   g_autoptr(MyApplication) app = my_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
 }
