c4fc54e269acb50602fce1247a2026df54e860d7
tiefseetauchner <lena@tauchner.me>
2026-06-28 18:28:17 +0200

    - fix paths

--- lib/ui/widgets/app_settings.dart
+++ lib/ui/widgets/app_settings.dart
@@ -92,7 +92,7 @@ class FeatureDisabledAppSetting extends ConsumerWidget {
       title: Text(displayText),
       subtitle: Text(context.tr("ProFeatureDisabled")),
       onTap: () => context.push(
-        "/buyproscreen",
+        "/disabledfeature",
         extra: DisabledFeatureScreenRouterExtra(feature: feature),
       ),
     );
