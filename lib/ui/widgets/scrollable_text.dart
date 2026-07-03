62328a021a5d8ca17179c2708fe186e58c56e527
tiefseetauchner <lena@tauchner.me>
2026-07-03 11:33:43 +0200

    - fix scroll calculations being independent of time

--- lib/ui/widgets/scrollable_text.dart
+++ lib/ui/widgets/scrollable_text.dart
@@ -58,6 +58,7 @@ class _ScrollableTextState extends ConsumerState<ScrollableText>
     with SingleTickerProviderStateMixin {
   Ticker? _ticker;
   double _scrollSpeed = 0;
+  Duration _lastElapsed = Duration.zero;
   Function? _onReachedEnd;
 
   MarkdownAst _ast = const MarkdownAst.empty();
@@ -69,6 +70,7 @@ class _ScrollableTextState extends ConsumerState<ScrollableText>
   void _startScrolling(double speed) {
     _stopScrolling();
     _scrollSpeed = speed;
+    _lastElapsed = Duration.zero;
     _ticker?.start();
   }
 
@@ -80,8 +82,19 @@ class _ScrollableTextState extends ConsumerState<ScrollableText>
   void initState() {
     super.initState();
 
+    ref.listen(_userScrollingProvider, (previous, next) {
+      if (next) {
+        _stopScrolling();
+      } else {
+        final prompter = ref.read(prompterProvider);
+        if (prompter.isPlaying) {
+          _startScrolling(prompter.speed);
+        }
+      }
+    });
+
     _ticker = createTicker((Duration elapsed) {
-      _tick();
+      _tick(elapsed);
     });
 
     _rebuildAst();
@@ -149,11 +162,16 @@ class _ScrollableTextState extends ConsumerState<ScrollableText>
     ref.read(currentChapterProvider.notifier).setValue(current);
   }
 
-  void _tick() {
+  void _tick(Duration elapsed) {
     final isUserScrolling = ref.read(_userScrollingProvider);
 
+    final deltaSeconds =
+        (elapsed - _lastElapsed).inMicroseconds /
+        Duration.microsecondsPerSecond;
+    _lastElapsed = elapsed;
+
     final calculatedScrollOffset =
-        (_scrollSpeed * (widget.style?.fontSize ?? 48)) / 10;
+        _getScrollOffsetInLinesPerSecond(_scrollSpeed) * deltaSeconds;
 
     if (widget.controller.scrollController.hasClients && !isUserScrolling) {
       if (widget.controller.scrollController.position.pixels +
@@ -162,14 +180,20 @@ class _ScrollableTextState extends ConsumerState<ScrollableText>
         _onReachedEnd?.call();
         return;
       }
-
-      widget.controller.scrollController.animateTo(
+      widget.controller.scrollController.jumpTo(
         widget.controller.scrollController.position.pixels +
             calculatedScrollOffset,
-        duration: Duration(milliseconds: 100),
-        curve: Curves.linear,
       );
     }
+  }
+
+  double _getScrollOffsetInLinesPerSecond(double speed) {
+    final textStyle = widget.style ?? const TextStyle(fontSize: 14);
+    final lineHeight = textStyle.height ?? 1.0;
+    final fontSize = textStyle.fontSize ?? 14.0;
+    final lineHeightInPixels = lineHeight * fontSize;
+
+    return speed * lineHeightInPixels;
   }
 
   @override
@@ -178,22 +202,6 @@ class _ScrollableTextState extends ConsumerState<ScrollableText>
     final mediaWidth = MediaQuery.of(context).size.width;
     final renderWidth = mediaWidth - widget.sideMargin * 2;
     _topPadding = mediaHeight;
-
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      widget.controller.scrollController.position.isScrollingNotifier
-          .addListener(() {
-            ref
-                .read(_userScrollingProvider.notifier)
-                .setValue(
-                  widget
-                      .controller
-                      .scrollController
-                      .position
-                      .isScrollingNotifier
-                      .value,
-                );
-          });
-    });
 
     final prompter = ref.watch(prompterProvider);
 
